7032901c7d29ff27c726e0bd837a983cd84b02fe
Woojin Lee <dhkep03@gmail.com>
2022-09-08 02:45:22 +0900

    This is coding test - 6

---
+++ BaekJoon/Dynamic_Programming/2579.py
@@ -0,0 +1,15 @@
+n = int(input())
+
+array = [0] * 301
+for i in range(n):
+    array[i + 1] = int(input())
+
+dp = [0] * 301
+dp[1] = array[1]
+dp[2] = array[1] + array[2]
+dp[3] = max(dp[1] + array[3], dp[2] + array[3])
+
+for i in range(3, n + 1):
+    dp[i] = max(dp[i - 3] + array[i - 1] + array[i], dp[i - 2] + array[i])
+
+print(dp[n])
