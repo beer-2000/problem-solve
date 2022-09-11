0be447ec07785bb14d98a5fb6af6fc890a19c655
Woojin Lee <dhkep03@gmail.com>
2022-09-12 00:49:37 +0900

    BaekJoon 2108, 10989

--- practice.py
+++ practice.py
@@ -1,103 +1,27 @@
-import time
+# import sys
 
-# n, m = map(int, input().split())
+# n = int(sys.stdin.readline().rstrip())
+# array = []
 
-# start_time = time.time()
-# numbers = [2]
+# for _ in range(n):
+#     array.append(int(sys.stdin.readline().rstrip()))
 
-# for i in range(3, m + 1):
-#     cnt = 0
-#     for number in numbers:
-#         if i % number == 0:
-#             cnt += 1
-#             break
-#     if cnt == 0:
-#         numbers.append(i)
+# for _ in range(n):
+#     for i in range(n - 1):
+#         if array[i] > array[i + 1]:
+#             array[i], array[i + 1] = array[i + 1], array[i]
 
-# cnt = 0
-# for i in range(n, m + 1):
-#     if i in numbers:
-#         print(i)
-# end_time = time.time()
-# print(end_time - start_time)
-
-
-
-# --------------------------------
-
-# n, m = map(int, input().split())
-
-# # start_time = time.time()
-# array = list(range(2, m + 1))
-# array = [2] + [i for i in array if i % 2 == 1]
-
-# index = 0
-# while True:
-#     array = array[0:index + 1] + [i for i in array[index + 1:] if i % array[index] != 0]
-#     index += 1
-#     if index >= len(array):
-#         break
-#     if array[index] >= m ** 0.5 + 1:
-#         break
-
-# result = [i for i in array if n <= i]
-# for number in result:
+# for number in array:
 #     print(number)
 
-# end_time = time.time()
-# print(end_time - start_time)
+import sys
+n = int(sys.stdin.readline().rstrip())
+count_array = [0] * 10001
 
+for i in range(n):
+    count_array[int(sys.stdin.readline().rstrip())] += 1
 
-# ---------------------------------
-
-# start_time = time.time()
-# def check_prime(number):
-#     if number == 1:
-#         return False
-#     else:
-#         for i in range(2, int(number ** 0.5 + 1)):
-#             if number % i == 0:
-#                 return False
-#         return True
-
-# n, m = map(int, input().split())
-
-# for i in range(n, m + 1):
-#     if check_prime(i) == True:
-#         print(i)
-# end_time = time.time()
-
-# print(end_time - start_time)
-
-
-def prime_list(n):
-    # 에라토스테네스의 체 초기화: n개 요소에 True 설정(소수로 간주)
-    sieve = [True] * n
-
-    # n의 최대 약수가 sqrt(n) 이하이므로 i=sqrt(n)까지 검사
-    m = int(n ** 0.5)
-    for i in range(2, m + 1):
-        if sieve[i] == True:           # i가 소수인 경우
-            for j in range(i+i, n, i): # i이후 i의 배수들을 False 판정
-                sieve[j] = False
-
-    # 소수 목록 산출
-    return [i for i in range(2, n) if sieve[i] == True]
-
-
-M, N = map(int, input().split())
-
-start_time = time.time()
-N += 1
-sieve = [True] * N
-for i in range(2, int(N**0.5)+1):
-    if sieve[i]:
-        for j in range(2*i, N, i):
-            sieve[j] = False
-for i in range(M, N):
-    if i > 1:
-        if sieve[i]:
+for i in range(10001):
+    if count_array[i] != 0:
+        for j in range(count_array[i]):
             print(i)
-
-end_time = time.time()
-print(end_time - start_time)
