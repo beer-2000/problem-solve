ae94cfbfdf7d38dd1a0e458b64346b8ba96a8396
Woojin Lee <dhkep03@gmail.com>
2022-10-04 23:37:41 +0900

    edit practice file

--- practice.py
+++ practice.py
@@ -5,19 +5,6 @@ a = set(map(int, sys.stdin.readline().split()))
 m = int(input())
 b = map(int, sys.stdin.readline().split())
 
-# def binary(l, N, start, end):
-#     if start > end:
-#         return 0
-#     m = (start+end)//2
-#     if l == N[m]:
-#         return 1
-#     elif l < N[m]:
-#         return binary(l, N, start, m-1)
-#     else:
-#         return binary(l, N, m+1, end)
-
-# for i in b:
-#     print(binary(i, a, 0, len(a) - 1))
 
 for i in b:
     print(1) if i in a else print(0)
